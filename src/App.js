d949fa237a54da570f4856b1d76cb5adf52f34b8
skgml0 <hnh2714@gmail.com>
2022-08-31 17:00:37 +0900

    Form-validation, control

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import ItemList from "./ItemList";
 
 
 function App() {
+   
     const [isPending, startTransition] = useTransition();
     const [boxCount, setBoxCount] = useState();
+    const [phoneNumber, setPhoneNumber] = useState();
+    const [message, setMessage] = useState();
 
     const handleUpdate = ({target}) => {
         // setBoxCount(target.value.length);
@@ -13,10 +16,39 @@ function App() {
             setBoxCount(target.value.length);
         });
     };
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert(phoneNumber);
+    }
+
+    const handleChange = (event)=> {
+      // set 과 시점차이가 존재한다.
+      if (event.target.value.startsWith(0)) {
+        setMessage('good');
+        setPhoneNumber(event.target.value);
+      }else if (event.target.value.length ===0){
+        setPhoneNumber("");
+        setMessage("");
+      }
+      else{
+        setPhoneNumber('');
+        setMessage('bad');
+      }
+
+    }
   return (
     <div className="App">
       <h2>React18</h2>
-      <input type="text" onChange={handleUpdate} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="phone">Phone Number:</label>
+        <br />
+        <input id="phone" onChange={handleChange} name="phone" value={phoneNumber}/>
+        <button type="submit" disabled={ phoneNumber?.length ===0 || message !== "good"}>Submit</button>     
+      </form>
+      <h3>{message}</h3>
+      <h3> {phoneNumber}</h3>
+        <input type="text" onChange={handleUpdate} />
+
         {isPending && <h1>Pending...</h1>}
         <ItemList items={boxCount} />
     </div>
