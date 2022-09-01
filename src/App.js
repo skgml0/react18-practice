36769fe5e536ec09ac5436c4408a9213d270c82b
skgml0 <hnh2714@gmail.com>
2022-09-01 16:41:44 +0900

    Data fetch

--- src/App.js
+++ src/App.js
@@ -1,73 +1,46 @@
 import './App.css';
 import React, { useState,useEffect } from 'react';
-// Key와 리렌더링 알아보기
-// key -> value 를 특정
-// ** key 없으면 버튼 그대로, 안에 값 다시 다 렌더링, 포커스만 그 자리 그대로  
-// ** key 있으면 컴포넌트 자체 위치만 바꿈 
-// Array.prototype.filter() 주어진 함수의 테스트를 통과하는 모든 요소를 모아 새로운 배열로 반환
-// Array.prototype.find() 주어진 판별 함수를 만족하는 첫 번째 요소의 값을 반환, 그런 요소가 없다면 undefined반환
-
-// (v,index)=> key={index}로 주었을 경우 한 값에 특정할 수 있는 index가 아니다. 
-// 항목들이 재배열 안되면 index 써도 o 
-// 그러나 재배열 된다면, 비효율적인 동작을 한다 
-// key는 중복없고, 바뀌지않는 값을 줘야 제대로 재사용 가능!
-const todos =[[
- {id:1, value: "Wash dishes1"},
- {id:2, value: "Wash dishes2"},
- {id:3, value: "Wash dishes3"},
- {id:5, value: "Wash dishes5"},
- {id:4, value: "Wash dishes4"},
-],[
-  {id:1, value: "Wash dishes1"},
-  {id:2, value: "Wash dishes2"},
-  {id:5, value: "Wash dishes5"},
-  {id:3, value: "Wash dishes3"},
-  {id:4, value: "Wash dishes4"},
- ],[
-   {id:3, value: "Wash dishes3"},
-  {id:1, value: "Wash dishes1"},
-  {id:2, value: "Wash dishes2"},
-  {id:4, value: "Wash dishes4"},
-  {id:5, value: "Wash dishes5"},
- ]
-];
+// slice(시작요소, 끝낼 요소 -1) 배열로 반환된다. 
+// {} 로 
+// Fetch api -> 네트워크 통신도구 
+// 상황별 핸들링 -> 1.데이터/2. 에러/3. 로딩  
 function App() {
+  const [data,setData] = useState(null);
+  const [error, setError] = useState(null);
 
-   const [items, setItems]= useState(todos[0]);
+  useEffect(()=> {
+    fetch('https://api.publicapis.org/entries').then(function (response)
+    {return response.json();
+    }).then(function (myJson){
+      setData(myJson);
+      console.log(data)
+    }).catch((error)=> {
+      setError(error);
+      alert(error);
+    })
+  },[]);
 
-   useEffect(()=>{
-    const interval = setInterval(()=> {
-      const random = Math.floor(Math.random() *3);
-      setItems(todos[random]);
-    },1000)
+  if (error !== null)
+  {
+    return <p>{error}</p>;
+  }
 
-    return () => {
-      clearInterval(interval);
-    }
-   },[])
-
-   const handleDoneClick = (todo) => {
-    console.dir(todo)
-    // items에 들어있는 todos를 하나씩 꺼냈을때 선택한 todo와 같지 않은 것들만 새 배열로 반환해서 저장
-    setItems((items) => items.filter((item)=> item != todo));
-   };
-   
-   const handleRestoreClick = () => {
-    setItems((items)=> [
-      ...items,
-      // 기존에 들어있는 items 중에 특정 item을 포함하고 있지않은 값을 반환해라 
-      todos.find((item)=> !items.includes(item))
-    ]);
-   };
+  if(data ===null) {
+    return <p style={{color:"red", fontSize:30}}>Loading...</p>
+  }
   return (
     <>
-      {items.map((todo)=> (
-        <div >
-          <span>{todo.value}</span>
-          <button onClick={()=> handleDoneClick(todo)}>Done</button>
+      <p>cat</p>
+      {/* JSON.stringify : 보기 좋은 형태로 바꿔주는것  */}
+      {/* <p>{JSON.stringify(data,null,2)}</p> */}
+
+      {data.entries.map((value,index)=> (
+        <div key={index}>
+        <p>API : {value.API}</p>
+        <p>DESCRIPTION : {value.Description}</p>
+        <br />
         </div>
       ))}
-      <button onClick={handleRestoreClick}>Restore</button>
     </>
   );
 }
