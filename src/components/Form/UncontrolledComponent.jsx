ddf60c3b961036b2b80f637604fa1cfabf99bf23
skgml0 <hnh2714@gmail.com>
2022-09-05 10:11:05 +0900

    Uncontrolled(비제어) 컴포넌트

---
+++ src/components/Form/UncontrolledComponent.jsx
@@ -0,0 +1,24 @@
+import React, {useRef} from 'react';
+// Uncontrolled는 값 자체는 모른다. 
+function UncontrolledComponent() {
+    const fileInputRef = useRef(null);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        alert(`Selected file - ${fileInputRef.current.files[0].name}`)
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Upload file: 
+                    <input type="file" ref={fileInputRef} />
+                </label>
+                <br />
+                <button type="submit" />
+            </form>
+        </div>
+    );
+}
+
+export default UncontrolledComponent;
